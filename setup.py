501aaeef709526b0d02c44979be3921c6c3d4ac9
Simon Krughoff <simon.krughoff@gmail.com>
2019-03-22 11:33:04 -0700

    Specify long description in markdown

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pip-install-test',
-      version='0.2',
+      version='0.3',
       description='A minimal stub package to test success of pip install',
       long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/lsst-sqre/sqre-pip_install_test',
       author='Simon Krughoff',
       author_email='krughoff@lsst.org',
