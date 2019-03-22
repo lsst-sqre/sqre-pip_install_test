fbb09463e13476078217b2852a8f23fb673861ae
Simon Krughoff <simon.krughoff@gmail.com>
2019-03-22 13:50:35 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pip-install-test',
-      version='0.3',
+      version='0.4',
       description='A minimal stub package to test success of pip install',
       long_description=long_description,
       long_description_content_type="text/markdown",
